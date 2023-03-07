9a0f8c61ad94ed1ff28f82205988eebacaefff8f
Sreehari23y04 <123545918+Sreehari23y04@users.noreply.github.com>
2023-03-07 13:46:47 +0530

    Add files via upload

---
+++ HelloWorld.plugin.js
@@ -0,0 +1,14 @@
+/**
+ * @name HelloworldPlugin
+ * @author FB
+ * @description To show Hello World alert message
+ * @version 0.0.1
+ */
+// Do stuff here before being called
+module.exports = meta => ({
+    start() {
+      BdApi.showConfirmationModal("Hello World", "This is just a basic plugin created by FB!");    },
+    stop() {
+      // Cleanup when disabled
+    }
+  });
